6035aa8ee10f96df3ca9a726934c85523f524e62
Abbey Hudson <a_hudson2@yahoo.com>
2017-09-07 17:45:04 -0700

    code cleanup

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,9 +1,10 @@
 var ClozeCard = function(cloze, full) {
   this.cloze = cloze;
   this.full = full;
+
   this.partial = function() {
-  	return this.full.replace(this.cloze, "...")
-  }
-}
+  	return this.full.replace(this.cloze, "...");
+  };
+};
  
 module.exports = ClozeCard;
